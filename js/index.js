59b27f4fc2dcac71ba45c1dd7b70ee4353967d8b
yanivkalfa <netameta1@yahoo.com>
2015-10-22 17:04:19 -0400

    removed cors

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 
   function getPresCount() {
     var deferred = $.Deferred();
-    $.ajax({ url: url, type: "get", crossDomain: true, dataType: 'json', error: deferred.reject, success: function(resp, status) {
+    $.ajax({ url: url, type: "get", dataType: 'json', error: deferred.reject, success: function(resp, status) {
       if (status === 'success' && resp.success) {
         deferred.resolve(resp.data.count || 0);
       } else {
