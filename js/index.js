8270ace6e7633b9919c87486112f30e143a8da5a
yanivkalfa <netameta1@yahoo.com>
2015-10-22 03:27:40 -0400

    change formatting a little

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
-(function($){
+(function($) {
   var updateFrequency=2000, interval, url = 'http://localhost:3000/api/v1/prescount';
 
-  function getPresCount(){
+  function getPresCount() {
     var deferred = $.Deferred();
-    $.ajax({ url: url, type:"get", dataType: 'json', error : deferred.reject, success: function(resp, status){
-      if(status === 'success' && resp.success){
+    $.ajax({ url: url, type: "get", dataType: 'json', error: deferred.reject, success: function(resp, status) {
+      if (status === 'success' && resp.success) {
         deferred.resolve(resp.data.count || 0);
-      }else{
+      } else {
         deferred.reject( resp );
       }
-    }});
+    } });
     return deferred.promise();
   }
 
-  function updateCounter(){
-    getPresCount().then( function(resp){
+  function updateCounter() {
+    getPresCount().then( function(resp) {
       $('#counter').html(resp);
     }, console.log );
   }
 
   interval = setInterval(updateCounter, updateFrequency);
-})(jQuery);
+}) (jQuery);
