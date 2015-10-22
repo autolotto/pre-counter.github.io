f0a9bebf5947798292b072073851c111de79f83a
yanivkalfa <netameta1@yahoo.com>
2015-10-22 03:16:53 -0400

    cleaned a little remove inline script, set temp values

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 (function($){
-  var ns, updateFrequency=2000, interval, hostName = '';
+  var ns, updateFrequency=2000, interval, hostName = 'http://localhost:3000/';
 
   ns = {
     services: {
-      presCount: hostName + '/api/v1/prescount'
+      presCount: hostName + 'api/v1/prescount'
     },
     utils: {
       getPresCount: function(){
         var deferred = $.Deferred();
         $.ajax({
-          url: this.services.presCount,
+          url: ns.services.presCount,
           type:"get",
           dataType: 'json',
           success: function (resp, status) {
-            if(status === 'success'){
-              deferred.resolve(resp );
+            if(status === 'success' && resp.success){
+              deferred.resolve(resp.data.count || 0);
             }else{
               deferred.reject( resp );
             }
@@ -25,11 +25,11 @@
       }
     },
     updateCounter: function(){
-      this.utils.getPresCount().then( function(resp){
+      ns.utils.getPresCount().then( function(resp){
         $('#counter').html(resp);
       }, console.log );
     }
   };
 
-  interval = setInterval($.proxy( ns.updateCounter, ns), updateFrequency);
+  interval = setInterval(ns.updateCounter, updateFrequency);
 })(jQuery);
